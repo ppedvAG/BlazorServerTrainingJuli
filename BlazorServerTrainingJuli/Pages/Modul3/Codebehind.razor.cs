cefad3e7b2eca2213b696836bf928ca0e1217797
HannesPreishuber <hannesp@ppedv.de>
2021-07-08 13:25:31 +0200

    Codebehind

---
+++ BlazorServerTrainingJuli/Pages/Modul3/Codebehind.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerTrainingJuli.Pages.Modul3
+{
+    public partial class Codebehind
+    {
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
