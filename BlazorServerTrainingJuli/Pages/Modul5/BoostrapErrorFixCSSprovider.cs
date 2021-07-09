97fda122a85b704b3cf152ae2ea5ea2d58fb6a01
HannesPreishuber <hannesp@ppedv.de>
2021-07-09 10:53:26 +0200

    FieldCssClassProvider um Boostrap Error korrekt anzuzeigen

---
+++ BlazorServerTrainingJuli/Pages/Modul5/BoostrapErrorFixCSSprovider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerTrainingJuli.Pages.Modul5
+{
+    public class BoostrapErrorFixCSSprovider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isValid ? "is-valid" : "is-invalid";
+        }
+    }
+}
---
+++ BlazorServerTrainingJuli/Pages/Modul5/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerTrainingJuli.Pages.Modul5
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [Range(150,200,ErrorMessage ="keine Achterbahn")]
+        public double Gross { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [StringLength(maximumLength:20,ErrorMessage ="reicht kurz 20 Zeichen")]
+        public string Name { get; set; }
+        public DateTime GebDat { get; set; }
+    }
+}
